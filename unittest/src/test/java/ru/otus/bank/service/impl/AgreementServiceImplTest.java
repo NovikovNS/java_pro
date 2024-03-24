38ea6fcaa9b8b362c7d92f29c2189e7803a59cdf
Nikita.Novikov <Nikita.Novikov@rt.ru>
2024-03-24 22:30:02 +0300

    [HW1] Added new tests. Updated calculating commission. Added AssertJ. Fixed naming for commission

--- unittest/src/test/java/ru/otus/bank/service/impl/AgreementServiceImplTest.java
+++ unittest/src/test/java/ru/otus/bank/service/impl/AgreementServiceImplTest.java
@@ -1,6 +1,5 @@
 package ru.otus.bank.service.impl;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
@@ -11,9 +10,13 @@ import ru.otus.bank.service.impl.AgreementServiceImpl;
 
 import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
 public class AgreementServiceImplTest {
 
-    private AgreementDao dao = Mockito.mock(AgreementDao.class);
+    private final AgreementDao dao = mock(AgreementDao.class);
 
     AgreementServiceImpl agreementServiceImpl;
 
@@ -34,8 +37,8 @@ public class AgreementServiceImplTest {
 
         Optional<Agreement> result = agreementServiceImpl.findByName(name);
 
-        Assertions.assertTrue(result.isPresent());
-        Assertions.assertEquals(10, agreement.getId());
+        assertThat(result.isPresent()).isTrue();
+        assertThat(agreement.getId()).isEqualTo(agreement.getId());
     }
 
     @Test
@@ -52,9 +55,28 @@ public class AgreementServiceImplTest {
 
         Optional<Agreement> result = agreementServiceImpl.findByName(name);
 
-        Assertions.assertEquals("test", captor.getValue());
-        Assertions.assertTrue(result.isPresent());
-        Assertions.assertEquals(10, agreement.getId());
+        assertThat(name).isEqualTo(captor.getValue());
+        assertThat(result.isPresent()).isTrue();
+        assertThat(agreement.getId()).isEqualTo(agreement.getId());
+    }
+
+    @Test
+    public void addingAgreement() {
+        // Test data
+        String agreementName = "testName";
+
+        // Invoke
+        agreementServiceImpl.addAgreement(agreementName);
+
+        // Captors
+        ArgumentCaptor<Agreement> captor = ArgumentCaptor.forClass(Agreement.class);
+
+        // Verifications
+        verify(dao).save(captor.capture());
+
+        assertThat(captor.getValue())
+            .matches(it -> it.getName().equals(agreementName))
+            .matches(it -> it.getId() == null);
     }
 
 }
