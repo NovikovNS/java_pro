0d3181cce1b68977500733d865c97072226d00b6
Nikita.Novikov <Nikita.Novikov@rt.ru>
2024-06-14 20:27:19 +0300

    Revert "[HW11] Created simple spring boot app for products"
    This reverts commit 4f310e04be37fe1a9d72996757e8579655742be5.

--- hw11_spring_boot/src/main/java/ru/otus/novikov/java/hw11/exceptions/EntityNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package ru.otus.novikov.java.hw11.exceptions;
-
-public class EntityNotFoundException extends EntityException {
-
-    public EntityNotFoundException(String message) {
-        super(message);
-    }
-}
--- hw11_spring_boot/src/main/java/ru/otus/novikov/java/hw11/util/Mapper.java
+++
@@ -1,6 +0,0 @@
-package ru.otus.novikov.java.hw11.util;
-
-public interface Mapper<E, D> {
-    D mapToDto(E entity);
-    E mapToEntity(D dto);
-}
